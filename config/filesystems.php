b9f501137c8da1e1416caec144166f11e6edd0b3
OzyPrazuganda <ozyprazuganda75@gmail.com>
2023-12-27 19:45:31 +0700

    filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,9 +38,9 @@ return [
 
         'public' => [
             'driver' => 'local',
-            // 'root' => '/home/sipgamam/public_html/storage',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
+            'root' => '/home/sipgamam/public_html/storage',
+            // 'root' => storage_path('app/public'),
+            // 'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
