1d6002813c789a2b7e5d109cccf5e13f7474de08
OzyPrazuganda <ozyprazuganda75@gmail.com>
2024-01-03 02:50:03 +0700

    update footer

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -246,36 +246,6 @@
     </section>
 
     <footer class="footer-59391">
-        <div class="border-bottom pb-5 mb-4">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-3">
-                        <form action="#" class="subscribe mb-4 mb-lg-0">
-                            <div class="form-group">
-                                <input type="email" class="form-control" placeholder="Enter your email">
-                                <button><span class="icon-keyboard_backspace"></span></button>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="col-lg-6 text-lg-center">
-                        <ul class="list-unstyled nav-links nav-left mb-4 mb-lg-0">
-                            <li><a href="#">Features</a></li>
-                            <li><a href="#">Blog</a></li>
-                            {{-- <li><a href="#">Pricing</a></li> --}}
-                            <li><a href="#">Services</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-3">
-                        <ul class="list-unstyled nav-links social nav-right text-lg-right">
-                            <li><a href="#"><span class="icon-twitter"></span></a></li>
-                            <li><a href="#"><span class="icon-instagram"></span></a></li>
-                            <li><a href="#"><span class="icon-facebook"></span></a></li>
-                            <li><a href="#"><span class="icon-pinterest"></span></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
 
         <div class="container">
             <div class="row align-items-center">
